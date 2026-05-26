a13b726d58a63d0579cc429b7331d023346a26a1
Molele Kishoez <molelekishoez@gmail.com>
2026-05-26 05:43:14 +0200

    Add new SVG icons for bakery, transaction, and visa; create Tailwind CSS configuration and TypeScript definitions

---
+++ app/layout.tsx
@@ -0,0 +1,44 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono, Inter, IBM_Plex_Sans, IBM_Plex_Serif } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const inter = Inter({subsets:['latin'],variable:'--font-inter'});
+const ibmPlexSerif = IBM_Plex_Serif({
+    subsets: ['latin'],
+    weight:['400', '700'],
+    variable: '--font-ibm-plex-serif'
+})
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Horizon",
+  description: "Horizon is a modern banking system for everyone",
+  icons: {
+    icon: '/icon/logo.svg'
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", inter.variable)}
+    >
+      <body className={`$inter.variable} ${ibmPlexSerif.variable}`}>{children}</body>
+    </html>
+  );
+}
